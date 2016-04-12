bf16adc8f8c028d389989bac1e23d25b7ef94df5
urbankos <urban.kos93@gmail.com>
2016-04-12 21:19:38 +0200

    ura
    asd

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jasa Jeler</title>
+
+<link rel="stylesheet" type="text/css" href="metrosex.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+	integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+	crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+	integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r"
+	crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+	integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+	crossorigin="anonymous"></script>
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  	
+  	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+
+</head>
+
+<body>
+<div class="ozadje">
+	<div class="zgornjiMeni col-md-12">
+	
+	</div>
+	<div class="col-md-3 nopadding">
+		<div class="meniIzbira center col-md-12">alo
+		</div>
+		<div class="meniIzbira center col-md-12">sperma
+		</div>
+		<div class="meniIzbira center col-md-12">peder
+		</div>
+		<div class="meniIzbira center col-md-12">homo
+		</div>
+	</div>
+	
+	<div class="vsebina col-md-6">
+			<div class="panel-body">
+				TEKST 
+			</div>
+	</div>
+	
+	<br/>
+	
+	<div class="sponzorji col-md-3">
+		<div class="sponzorji1 col-md-12">
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
